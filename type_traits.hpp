890fa1ee81817df45be25ae93544bf73be3df4dd
Jungcow <jungwkim@student.42seoul.kr>
2022-05-07 17:48:43 +0900

    feat: add is_integral struct

--- type_traits.hpp
+++ type_traits.hpp
@@ -3,13 +3,19 @@
 
 #include <iostream>
 
+#include "iterator.hpp"
+
 namespace ft
 {
-	struct true_type {};
-	struct false_type {};
+	struct input_iterator_tag;
+	struct true_type {
+	};
+	struct false_type {
+	};
 
 	template <bool, class T = void>
-	struct enable_if {};
+	struct enable_if {
+	};
 
 	template <class T>
 	struct enable_if<true, T> {
@@ -17,22 +23,98 @@ namespace ft
 		typedef T type;
 	};
 
-
 	template <typename, typename>
 	struct is_same {
 		enum { value = 0 };
 		typedef false_type type;
 	};
-
 	template <typename T>
 	struct is_same<T, T> {
 		enum { value = 1 };
 		typedef true_type type;
 	};
-	// is_integral
-	// is_convertible
-	// interal_constant ??
 
+	template <typename T>
+	struct is_integral {
+		enum { value = 0 };
+		typedef false_type type;
+	};
+
+	template <>
+	struct is_integral<bool> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
+	template <>
+	struct is_integral<char> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
+	template <>
+	struct is_integral<char16_t> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
+	template <>
+	struct is_integral<char32_t> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
+	template <>
+	struct is_integral<wchar_t> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
+	template <>
+	struct is_integral<signed char> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
+	template <>
+	struct is_integral<short int> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
+	template <>
+	struct is_integral<int> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
+	template <>
+	struct is_integral<long int> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
+	template <>
+	struct is_integral<long long int> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
+	template <>
+	struct is_integral<unsigned char> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
+	template <>
+	struct is_integral<unsigned short int> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
+	template <>
+	struct is_integral<unsigned int> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
+	template <>
+	struct is_integral<unsigned long int> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
+	template <>
+	struct is_integral<unsigned long long int> {
+		enum { value = 1 };
+		typedef true_type type;
+	};
 }
 
 #endif
