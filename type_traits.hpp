d0a6f440c9e1b8ed8943f4f3fcffe0a62479de95
Jungcow <jungwkim@student.42seoul.kr>
2022-07-07 02:36:19 +0900

    feat: add several type trait utils
    - add is_arithmetic
    - add is_base_of
    - add is_pointer and is_array
    - add is_trivial_destructible_junior a light version of origin
    - refactor is_integral to use is_same util

--- type_traits.hpp
+++ type_traits.hpp
@@ -2,8 +2,14 @@
 #define __FT_TYPE_TRAITS_H__
 
 /**
- * is_integral
  * enable_if
+ * is_same
+ * is_integral
+ * is_arithmetic
+ * is_base_of
+ * is_pointer
+ * is_array
+ * is_trivial_destructible_junior
  */
 
 #include <cstdint>
@@ -25,91 +31,6 @@ namespace ft
 	};
 
 	/**
-	 * is_integral
-	 */
-	template <typename T>
-	struct is_integral
-	{
-		static const bool value = false;
-	};
-
-	template <>
-	struct is_integral<bool>
-	{
-		static const bool value = true;
-	};
-	template <>
-	struct is_integral<char>
-	{
-		static const bool value = true;
-	};
-	template <>
-	struct is_integral<char16_t>
-	{
-		static const bool value = true;
-	};
-	template <>
-	struct is_integral<char32_t>
-	{
-		static const bool value = true;
-	};
-	template <>
-	struct is_integral<wchar_t>
-	{
-		static const bool value = true;
-	};
-	template <>
-	struct is_integral<signed char>
-	{
-		static const bool value = true;
-	};
-	template <>
-	struct is_integral<short int>
-	{
-		static const bool value = true;
-	};
-	template <>
-	struct is_integral<int>
-	{
-		static const bool value = true;
-	};
-	template <>
-	struct is_integral<long int>
-	{
-		static const bool value = true;
-	};
-	template <>
-	struct is_integral<long long int>
-	{
-		static const bool value = true;
-	};
-	template <>
-	struct is_integral<unsigned char>
-	{
-		static const bool value = true;
-	};
-	template <>
-	struct is_integral<unsigned short int>
-	{
-		static const bool value = true;
-	};
-	template <>
-	struct is_integral<unsigned int>
-	{
-		static const bool value = true;
-	};
-	template <>
-	struct is_integral<unsigned long int>
-	{
-		static const bool value = true;
-	};
-	template <>
-	struct is_integral<unsigned long long int>
-	{
-		static const bool value = true;
-	};
-
-	/**
 	 * is_same
 	 */
 	template <typename T, typename U>
@@ -122,6 +43,91 @@ namespace ft
 	struct is_same<T, T>
 	{
 		static const bool value = true;
+	};
+
+	/**
+	 * is_integral
+	 */
+	template <typename T>
+	struct is_integral
+	{
+		static const bool value = ft::is_same<T, bool>::value ||
+								  ft::is_same<T, char>::value ||
+								  ft::is_same<T, char16_t>::value ||
+								  ft::is_same<T, char32_t>::value ||
+								  ft::is_same<T, wchar_t>::value ||
+								  ft::is_same<T, signed char>::value ||
+								  ft::is_same<T, short int>::value ||
+								  ft::is_same<T, int>::value ||
+								  ft::is_same<T, long int>::value ||
+								  ft::is_same<T, long long int>::value ||
+								  ft::is_same<T, unsigned char>::value ||
+								  ft::is_same<T, unsigned short int>::value ||
+								  ft::is_same<T, unsigned int>::value ||
+								  ft::is_same<T, unsigned long int>::value ||
+								  ft::is_same<T, unsigned long long int>::value;
+	};
+
+	/**
+	 * is_arithmetic
+	 */
+	template <typename T>
+	struct is_arithmetic
+	{
+		typedef T type;
+		static const bool value = ft::is_integral<T>::value ||
+								  ft::is_same<T, float>::value ||
+								  ft::is_same<T, double>::value ||
+								  ft::is_same<T, long double>::value;
+	};
+
+	/**
+	 * is_base_of
+	 */
+	template <class Base, class Derived>
+	struct is_base_of
+	{
+		struct two
+		{
+			char c[2];
+		};
+		static two test_base(Base*);
+		static char test_base(...);
+
+		static const bool value = sizeof(test_base(static_cast<Derived*>(0))) == 2;
+	};
+
+	/**
+	 * is_pointer
+	 */
+	template <typename T>
+	struct is_pointer
+	{
+		static const bool value = false;
+	};
+	template <typename T>
+	struct is_pointer<T*>
+	{
+		static const bool value = true;
+	};
+
+	template <typename T>
+	struct is_array
+	{
+		static const bool value = false;
+	};
+	template <typename T>
+	struct is_array<T[]>
+	{
+		static const bool value = true;
+	};
+
+	template <typename T>
+	struct is_trivial_destructible_junior
+	{
+		static const bool value = ft::is_arithmetic<T>::value ||
+								  ft::is_pointer<T>::value ||
+								  ft::is_array<T>::value;
 	};
 }
 
