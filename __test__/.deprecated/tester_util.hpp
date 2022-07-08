42ebc409e0b01588d2c4312095c2bfdbc76c52db
Jungcow <jungwkim@student.42seoul.kr>
2022-07-08 16:06:41 +0900

    chore: remove unnecessary directory

--- __test__/.deprecated/tester_util.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __TESTER_UTIL_H__
-# define __TESTER_UTIL_H__
-
-
-
-#endif
--- utils/cmake_example/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(void)
-{
-	cout << "cmake examle" << std::endl;
-	return (0);
-}
