8a58a37b475e20f6e87e502bede5434fcb9ab882
Jungcow <jungwkim@student.42seoul.kr>
2022-07-08 17:22:29 +0900

    #2 feat: add member type
    - value_type
    - container_type
    - size_type

--- stack.hpp
+++ stack.hpp
@@ -1,6 +1,15 @@
 #ifndef __FT_STACK_H__
 #define __FT_STACK_H__
 
+#include "vector.hpp"
 
+template <class T, class Container = ft::vector<T> >
+class stack
+{
+private:
+	typedef T value_type;
+	typedef Container container_type;
+	typedef typename Container::size_type size_type;
+};
 
 #endif
