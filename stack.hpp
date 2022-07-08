7eff324851851febf508ee27881b1fd2a0528a08
Jungcow <jungwkim@student.42seoul.kr>
2022-07-08 17:48:08 +0900

    #4 feat: add non-member functions

--- stack.hpp
+++ stack.hpp
@@ -3,49 +3,96 @@
 
 #include "vector.hpp"
 
-template <class T, class Container = ft::vector<T> >
-class stack
+namespace ft
 {
-private:
-	typedef T value_type;
-	typedef Container container_type;
-	typedef typename container_type::size_type size_type;
 
-private:
-	container_type base_;
-
-public:
-	explicit stack(const container_type& cntr = container_type())
-		: base_(cntr) {}
-
-	bool empty() const
+	template <class T, class Container = ft::vector<T> >
+	class stack
 	{
-		return (base_.empty());
-	}
+	private:
+		typedef T value_type;
+		typedef Container container_type;
+		typedef typename container_type::size_type size_type;
 
-	void pop()
-	{
-		return (base_.pop_back());
-	}
+	private:
+		container_type base_;
 
-	void push(const value_type& val)
-	{
-		return (base_.push_back(val));
-	}
+	public:
+		explicit stack(const container_type& cntr = container_type())
+			: base_(cntr) {}
 
-	size_type size() const
-	{
-		return (base_.size());
-	}
+		bool empty() const
+		{
+			return (base_.empty());
+		}
 
-	value_type& top()
+		void pop()
+		{
+			return (base_.pop_back());
+		}
+
+		void push(const value_type& val)
+		{
+			return (base_.push_back(val));
+		}
+
+		size_type size() const
+		{
+			return (base_.size());
+		}
+
+		value_type& top()
+		{
+			return (base_.back());
+		}
+		const value_type& top() const
+		{
+			return (base_.back());
+		}
+
+		template <class U, class Cont>
+		friend bool operator==(const stack<U, Cont>& lhs, const stack<U, Cont>& rhs);
+		template <class U, class Cont>
+		friend bool operator!=(const stack<U, Cont>& lhs, const stack<U, Cont>& rhs);
+		template <class U, class Cont>
+		friend bool operator<(const stack<U, Cont>& lhs, const stack<U, Cont>& rhs);
+		template <class U, class Cont>
+		friend bool operator<=(const stack<U, Cont>& lhs, const stack<U, Cont>& rhs);
+		template <class U, class Cont>
+		friend bool operator>(const stack<U, Cont>& lhs, const stack<U, Cont>& rhs);
+		template <class U, class Cont>
+		friend bool operator>=(const stack<U, Cont>& lhs, const stack<U, Cont>& rhs);
+	};
+	template <class U, class Cont>
+	bool operator==(const stack<U, Cont>& lhs, const stack<U, Cont>& rhs)
 	{
-		return (base_.back());
+		return (lhs.base_ == rhs.base_);
 	}
-	const value_type& top() const
+	template <class U, class Cont>
+	bool operator!=(const stack<U, Cont>& lhs, const stack<U, Cont>& rhs)
 	{
-		return (base_.back());
+		return !(lhs == rhs);
 	}
-};
+	template <class U, class Cont>
+	bool operator<(const stack<U, Cont>& lhs, const stack<U, Cont>& rhs)
+	{
+		return (lhs.base_ < rhs.base_);
+	}
+	template <class U, class Cont>
+	bool operator<=(const stack<U, Cont>& lhs, const stack<U, Cont>& rhs)
+	{
+		return !(rhs < lhs);
+	}
+	template <class U, class Cont>
+	bool operator>(const stack<U, Cont>& lhs, const stack<U, Cont>& rhs)
+	{
+		return (rhs < lhs);
+	}
+	template <class U, class Cont>
+	bool operator>=(const stack<U, Cont>& lhs, const stack<U, Cont>& rhs)
+	{
+		return !(lhs < rhs);
+	}
+}
 
 #endif
